0e08c57120669b536c43f79e23f42e393b5749bc
Saloni Tanmor <saloni.tanmor1@gmail.com>
2024-09-29 19:01:18 +0530

    configuration

---
+++ apps/test.py
@@ -0,0 +1,26 @@
+# import sqlite3
+
+# Connect to the SQLite database (or create it if it doesn't exist)
+# connection = sqlite3.connect('db.sqlite3')
+
+# Create a cursor object
+# cursor = connection.cursor()
+
+# Create a table (if it doesn't exist)
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS users (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     age INTEGER
+# )
+# ''')
+
+# Insert values into the table
+# username = 'John Doe'
+# password = 12345
+
+# cursor.execute('INSERT INTO Users (username, password) VALUES (?, ?)', (username, password))
+
+# Commit the changes and close the connection
+# connection.commit()
+# connection.close()
